55077aa3877510b9f8fd93f91867a17dee8795d1
colecrum26 <cwcrumrine@gmail.com>
2023-03-15 12:26:19 -0500

    working exercise 2 bonus

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
 console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+// console.log("EXERCISE 1:\n==========\n");
 
-function printOdds(count) {
-    if (count < 0){
-        console.log("Please enter a positive number.");
-        return;
-    }
-  for (let i = 0; i <= count; i++) {
-    if (i % 2 == 1){
-      console.log(i);
-    }
-  }
-}
-
-printOdds(-30);
-
-// Exercise 2 Section
-// console.log("EXERCISE 2:\n==========\n");
-
-// function checkAge(userName, age){
-//     let aboveSixteen = `Congrats ${userName}, you can drive!`
-//     let belowSixteen = `Sorry ${userName}, but you need to wait until you're 16.`
-//     if (age < 16) {
-//         console.log(belowSixteen);
-//     } else {
-//         console.log(aboveSixteen);
+// function printOdds(count) {
+//     if (count < 1){
+//         console.log("Please enter a positive number.");
+//         return;
 //     }
+//   for (let i = 0; i <= count; i++) {
+//     if (i % 2 == 1){
+//       console.log(i);
+//     }
+//   }
 // }
 
-// checkAge("JPJ", 17)
+// printOdds(-30);
+
+// Exercise 2 Section
+console.log("EXERCISE 2:\n==========\n");
+
+function checkAge(userName, age){
+    let aboveSixteen = `Congrats ${userName}, you can drive!`
+    let belowSixteen = `Sorry ${userName}, but you need to wait until you're 16.`
+    if (userName, age) {
+        console.log("Please enter your name and age.")
+    }
+    else if (age < 16) {
+        console.log(belowSixteen);
+    } else {
+        console.log(aboveSixteen);
+    }
+}
+
+checkAge()
