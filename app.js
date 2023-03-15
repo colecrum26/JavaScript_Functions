e2a2191f3e5cbd39ad16c23855b1de5ed69d61c4
colecrum26 <cwcrumrine@gmail.com>
2023-03-15 11:32:08 -0500

    exercise one

--- app.js
+++ app.js
@@ -3,5 +3,16 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+function printOdds(count) {
+  for (let count = 1; count <= 100; count++) {
+    if (count % 2 === 0);
+    else {
+      console.log(`${count}`);
+    }
+  }
+}
+
+printOdds();
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
