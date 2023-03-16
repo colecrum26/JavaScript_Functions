7447e9e73538b62bb5c69e3f6764529d5e560f16
colecrum26 <cwcrumrine@gmail.com>
2023-03-15 19:55:50 -0500

    worked on bonus

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function printOdds(count) {
   }
 }
 
-printOdds(30);
+// printOdds(30);
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
@@ -23,16 +23,18 @@ console.log("EXERCISE 2:\n==========\n");
 function checkAge(userName, age) {
   let aboveSixteen = `Congrats ${userName}, you can drive!`;
   let belowSixteen = `Sorry ${userName}, but you need to wait until you're 16.`;
-  // if (userName, age) {
-  //     console.log("Please enter your name and age.")
-  if (age < 16) {
-    console.log(belowSixteen);
+  if ((userName, age)) {
+    if (age < 16) {
+      console.log(belowSixteen);
+    } else {
+      console.log(aboveSixteen);
+    }
   } else {
-    console.log(aboveSixteen);
+    console.log("Please enter both your name and age.");
   }
 }
 
-checkAge("JPJ", 21);
+// checkAge("JPJ", 21);
 
 // Exercise 3 Section
 console.log("EXERCISE 3:\n==========\n");
@@ -62,10 +64,10 @@ function whichQuadrant(x, y) {
   }
 }
 
-whichQuadrant(-1, 5);
+// whichQuadrant(-1, 5);
 
 // Exercise 4 Section
-console.log("EXERCISE 3:\n==========\n");
+console.log("EXERCISE 4:\n==========\n");
 
 function triangle(a, b, c) {
   if (a + b <= c || b + c <= a || c + a <= b) {
@@ -79,4 +81,37 @@ function triangle(a, b, c) {
   }
 }
 
-triangle(3, 3, 3);
+// triangle(3, 3, 3);
+
+// BONUS Exercise 5 Section
+console.log("BONUS EXERCISE 5:\n==========\n");
+
+function planStatus(planLimit, day, usage) {
+  let plan = 30;
+  let daysRem = plan - day;
+  let avgUse = planLimit / plan;
+  let dayUse = usage / day;
+  let dataRem = planLimit - usage;
+  let planRem = dataRem / day;
+  let expUsage = dayUse * plan - planLimit;
+  console.log(`${day} days used, ${daysRem} days remaining`);
+  console.log(`Average expected daily use: ${avgUse} GB/day`);
+  if (dayUse > avgUse && usage < planLimit) {
+    console.log(
+      `You are EXCEEDING the average expected daily use at ${dayUse} GB/day. You have ${dataRem} GB left for the plan. Continuing this high usage, 
+you'll exceed your data plan by ${expUsage} GB. To stay below your plan, use no more than ${planRem} GB/day.`);
+  } else if (dayUse == avgUse) {
+    console.log(
+      `You are RIGHT ON the average daily use under the plan. You have ${dataRem} GB left for the plan. Continue this usage to stay in line with the data plan.`);
+  } else if (dayUse < avgUse) {
+    console.log(
+      `You are BELOW the average daily use. You have ${dataRem} GB left for the plan. You may continue at the current usage or move up to ${planRem} GB/day.`
+    );
+  } else {
+    console.log(
+      "You are out of data for the plan period. Your phone will self-destruct in 3... 2... 1..."
+    );
+  }
+}
+
+planStatus(100, 15, 25);
